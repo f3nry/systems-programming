1ddb9c629a57d199bbd1381cb104beda2ad69ee6
Paul Henry <letuboy@gmail.com>
2018-01-19 10:42:31 -0800

    use tabs instead of spaces and introduce seeded randomness in quicksort.go

--- algorithms/quicksort.go
+++ algorithms/quicksort.go
@@ -1,72 +1,75 @@
 package main
 
 import (
-    "fmt"
-    "os"
-    "strconv"
-    "log"
-    "math/rand"
+	"fmt"
+	"log"
+	"math/rand"
+	"os"
+	"strconv"
+	"time"
 )
 
 func swap(array []int, index1 int, index2 int) {
-    swap := array[index1]
-    array[index1] = array[index2]
-    array[index2] = swap
+	swap := array[index1]
+	array[index1] = array[index2]
+	array[index2] = swap
 }
 
 func partition(array []int, low int, high int) int {
-    if low < high {
-        pivot := array[high]
-        i := low
+	if low < high {
+		pivot := array[high]
+		i := low
 
-        for i < high {
-            if array[i] < pivot {
-                i++
-            } else {
-                swap(array, i, high - 1)
-                swap(array, high - 1, high)
-                high -= 1
-            }
-        }
-    }
+		for i < high {
+			if array[i] < pivot {
+				i++
+			} else {
+				swap(array, i, high-1)
+				swap(array, high-1, high)
+				high -= 1
+			}
+		}
+	}
 
-    return high
+	return high
 }
 
 func recurQuickSort(array []int, low int, high int) {
-    if low < high {
-        p := partition(array, low, high)
-        recurQuickSort(array, low, p - 1)
-        recurQuickSort(array, p + 1, high)
-    }
+	if low < high {
+		p := partition(array, low, high)
+		recurQuickSort(array, low, p-1)
+		recurQuickSort(array, p+1, high)
+	}
 }
 
 func quickSort(array []int) {
-    recurQuickSort(array, 0, len(array) - 1)
+	recurQuickSort(array, 0, len(array)-1)
 }
 
 func makeRandomArray(desiredArrayLength int) []int {
-    array := make([]int, desiredArrayLength)
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
 
-    for index, _ := range array {
-        array[index] = rand.Intn(desiredArrayLength)
-    }
+	array := make([]int, desiredArrayLength)
 
-    return array
+	for index, _ := range array {
+		array[index] = r.Intn(desiredArrayLength)
+	}
+
+	return array
 }
 
 func main() {
-    desiredArrayLength, err := strconv.Atoi(os.Args[1])
+	desiredArrayLength, err := strconv.Atoi(os.Args[1])
 
-    if err != nil {
-        log.Fatal(err)
-    }
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    array := makeRandomArray(desiredArrayLength)
+	array := makeRandomArray(desiredArrayLength)
 
-    fmt.Println("Initial Array", array)
+	fmt.Println("Initial Array", array)
 
-    quickSort(array)
+	quickSort(array)
 
-    fmt.Println("Sorted Array", array)
+	fmt.Println("Sorted Array", array)
 }
